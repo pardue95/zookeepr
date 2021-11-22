57539729982c9fa49a17321a2eabb0c2f274081a
pardue95 <85760640+pardue95@users.noreply.github.com>
2021-11-22 15:24:26 -0600

    Handle Requests for a Specific Animal

--- server.js
+++ server.js
@@ -40,6 +40,13 @@ function filterByQuery(query, animalsArray) {
     }
     // return the filtered results:
     return filteredResults;
+
+    
+  }
+
+  function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
   }
 
   app.get('/api/animals', (req, res) => {
@@ -48,6 +55,15 @@ function filterByQuery(query, animalsArray) {
       results = filterByQuery(req.query, results);
     }
     res.json(results);
+  });
+
+  app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+      res.json(result);
+    } else {
+      res.sendStatus(404);
+    }
   });
 
   app.listen(PORT, () => {
